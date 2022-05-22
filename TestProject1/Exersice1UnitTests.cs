3c0e96e454fb2ab33d264587f14de91ee437d1b3
amal shalayel <amal-shalayel@hotmail.com>
2022-05-22 21:57:47 +0300

    excersice 1

---
+++ TestProject1/Exersice1UnitTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using SimpleApp;
+
+namespace TestProject1
+{
+    public class Exersice1UnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces("") == "");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces(" ") == " ");
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces("   ") == " ");
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces("a") == "a");
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces(" a") == " a");
+        }
+
+        [Test]
+        public void Test6()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces("a ") == "a ");
+        }
+
+        [Test]
+        public void Test7()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces("a a") == "a a");
+        }
+
+        [Test]
+        public void Test8()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces("   a") == " a");
+        }
+
+        [Test]
+        public void Test9()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces("a   ") == "a ");
+        }
+
+        [Test]
+        public void Test10()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces("a   a") == "a a");
+        }
+
+        [Test]
+        public void Test11()
+        {
+            Assert.IsTrue(Exersice1.ReplaceWhiteSpaces("hello     world   test") == "hello world test");
+        }
+
+
+
+    }
+}
---
+++ UnitTestProject/ReplaceBlanksUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ReplaceBlanksUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var expected =    ReplaceWhiteSpaces.re
+            //Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+
+
+        }
+    }
+}
