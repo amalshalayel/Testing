2574c209be3cffb2056b635204aacfe37d15c17d
amal shalayel <amal-shalayel@hotmail.com>
2022-05-22 21:57:03 +0300

    code

---
+++ SimpleApp/Exersice1.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SimpleApp
+{
+  public  class Exersice1
+    {
+        private static readonly Regex regex = new Regex(@"\s+");
+
+        public static string ReplaceWhiteSpaces(string str)
+        {
+            return regex.Replace(str, " ");
+        }
+    }
+}
---
+++ SimpleApp/Exersice2.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SimpleApp
+{
+  public  class Exersice2
+    {
+        public static string TriangleType(int side1, int side2, int side3)
+        {
+            if (side1 == side2 && side2 == side3)
+            {
+                return "Equilateral";
+            }
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+            {
+                return "Isosceles";
+            }
+            else
+            {
+                return "Scalene";
+            }
+        }
+    }
+
+}
